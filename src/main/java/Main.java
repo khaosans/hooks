63b8360cca7d6dff1ad614cc69d09f6f27030664
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-05-22 20:32:03 -0700

    added database connection from twitter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import db.Posty;
+import tweets.GetStatusTweet;
+
+import java.sql.SQLException;
+import java.util.concurrent.*;
+
 /**
  * Created by souriyakhaosanga on 5/20/14.
  * Copyright © 2014 Urban Airship and Contributors
  */
 public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, SQLException {
+        Callable<String> callable = new GetStatusTweet();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<String> future = executor.submit(callable);
+
+        System.out.println(future.get());
+
+        new Posty();
+        Posty.writeToDb(future.get());
+
+    }
+
 }
